9b47336e48afbb914fdfaa41358f419be76fe077
Bhagyajeet Borah <borahbhagya040@gmail.com>
2024-04-29 00:25:14 +0530

    minor changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@
 export const environment = {
     production: false,
     API_KEY: '2958ac470ff34c3ebc8174321242604',
-    apiUrl: 'http://api.weatherapi.com/v1/forecast.json'
+    apiUrl: 'https://api.weatherapi.com/v1/forecast.json'
 };
   
