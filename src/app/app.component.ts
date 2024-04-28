4f59761c16094fc2a70560bf856e3e674e48d9e8
Bhagyajeet Borah <borahbhagya040@gmail.com>
2024-04-28 17:14:09 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'weatherApp';
+
+export class AppComponent implements OnInit{
+  cityName: string = 'Guwahati';
+  weatherData: any;
+  upcomingForecast: any;
+  API_URL = environment.apiUrl;
+  API_KEY = environment.API_KEY
+
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.fetchWeatherData(this.cityName);
+    // this.fetchUpcomingForecast(this.cityName);
+  }
+  search() {
+    if (this.cityName.trim() !== '') {
+      // Search only if the input is not empty
+      this.fetchWeatherData(this.cityName);
+      // this.fetchUpcomingForecast(this.cityName);
+    }
+  }
+
+  fetchWeatherData(city: string) {
+    this.http.get(`${this.API_URL}?key=${this.API_KEY}&q=${city}&days=4`)
+      .subscribe((response: any) => {
+        console.log(response);
+        // response.location.name
+        this.weatherData = response;
+        this.upcomingForecast = response.forecast.forecastday.slice(1);
+      });
+  }
+
+  // fetchUpcomingForecast(city: string) {
+  //   this.http.get(`http://api.weatherapi.com/v1/forecast.json?key=${this.API_KEY}&q=${city}&days=3`)
+  //     .subscribe((response: any) => {
+  //       // response.location.name
+  //       this.upcomingForecast = response;
+  //     });
+  // }
+
+  timeToDate(day: any) {
+    const date = new Date(day);
+    const dayName = date.toLocaleString('en-US', { weekday: 'long' });
+    return dayName;
+  }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+// environment.prod.ts
+export const environment = {
+    production: true,
+    API_KEY: '2958ac470ff34c3ebc8174321242604',
+    apiUrl: 'http://api.weatherapi.com/v1/forecast.json'
+  };
+  
